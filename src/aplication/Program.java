67e4700f69b5dbb4f7d10afb529e1c28aa96be16
RichardOrlandini <richardorlandini09@gmail.com>
2023-02-28 16:31:02 -0300

    created class Board and Piece

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,12 +1,13 @@
 package aplication;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
     public static void main(String[] args) {
 
         var position = new Position(3,10);
-
         System.out.println(position);
+        var board = new Board(8,8);
     }
 }
